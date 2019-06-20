ffeb87a887cd0967f5cfdbfc2564d170f5e55f2f
xdavidos <david.fitzek@protonmail.com>
2019-06-20 14:52:04 +0200

    add num of steps to train n epochs

--- src/RL.py
+++ src/RL.py
@@ -427,7 +427,8 @@ class RL():
                                                                                                                                                                         prediction_list_p_error=prediction_list_p_error, 
                                                                                                                                                                         nbr_of_qubit_errors=int(self.system_size/2)+1,
                                                                                                                                                                         directory_path=predict_directory_path,
-                                                                                                                                                                        save_prediction=True)
+                                                                                                                                                                        save_prediction=True,
+                                                                                                                                                                        num_of_steps=50)
 
             data_all = np.append(data_all, np.array([[self.system_size, self.network_name, i+1, self.replay_memory, self.device, self.learning_rate, target_update, optimizer,target_value_definition, reward_definition, 
                 self.discount_factor, training_steps * (i+1), mean_q_list[0], prediction_list_p_error[0], num_of_predictions, len(failed_syndroms)/2, error_corrected_list[0], ground_state_list[0], average_number_of_steps_list[0],failure_rate, self.p_error]]), axis=0)

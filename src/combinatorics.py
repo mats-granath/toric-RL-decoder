f51d22ec5ea6b8188cb5c2c742307361a48bedde
David <david.fitzek@protonmail.com>
2019-11-11 13:41:51 +0100

    change p_l_alg

--- src/combinatorics.py
+++ src/combinatorics.py
@@ -15,6 +15,13 @@ number_of_ways = 0
 for n_y in range(k):
     number_of_ways += comb(k,n_y) * comb(d, k-n_y) * comb(d-k+n_y, n_y)
 
+test_ways = 0 
+for n_y in range(3):
+    test_ways += 1/(n_y+1) * comb(k,n_y) * comb(d, k-n_y) * comb(d-k+n_y, n_y)
+p_l_mwpm = 2 * 2 * d * test_ways * ((2/3)*p)**k # 1/3 for only x errors, 2/3 for all three error types  
+print(p_l_mwpm, 'p_l_mwpm') 
+
+
 # probability logical error MWPM 
 p_l_mwpm = 2 * 2 * d * number_of_ways * ((2/3)*p)**k # 1/3 for only x errors, 2/3 for all three error types  
 print(p_l_mwpm, 'p_l_mwpm') 
@@ -26,7 +33,7 @@ print(p_l_mwpm, 'p_l_mwpm')
 # probability logical error RL 
 #print(p_l_alg, 'fail rate alg')
 #p_l_alg = d * (p/3)**k * ( 2 * comb(d,k) + k * comb(d, k-1))
-p_l_alg = 4 * d * (comb(d,k) * (p/3)**k + k * comb(d, k-1) * ((2/3)*p)**(k-1))
+p_l_alg = 4 * d * (comb(d,k) * (p/3)**k + k * (p/3) * comb(d, k-1) * ((2/3)*p)**(k-1))
 print(p_l_alg, 'pl_alg')
 
 # fail rate for three operators 
